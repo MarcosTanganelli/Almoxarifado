bd3aa85fdc156f3f52f3bb03cbf097a9e1359fe2
Àgora <marcos.vtl@hotmail.com>
2023-04-19 21:25:45 -0300

    1°Adicionado as janelas

--- almoxarifado.py
+++ almoxarifado.py
@@ -1,5 +1,26 @@
 import tkinter as tk
 import mysql.connector
+def open_window_1():
+    window = tk.Toplevel()
+    window.title("Inserir")
+    window.geometry("200x100")
+
+    label = tk.Label(window, text="Local de inserir")
+    label.pack()
+
+    button_close = tk.Button(window, text="Fechar", command=window.destroy)
+    button_close.pack()
+
+def open_window_2():
+    window = tk.Toplevel()
+    window.title("Remover")
+    window.geometry("200x100")
+
+    label = tk.Label(window, text="Local de Remover")
+    label.pack()
+
+    button_close = tk.Button(window, text="Fechar", command=window.destroy)
+    button_close.pack()
 
 conexao = mysql.connector.connect(
     host="localhost",
@@ -16,16 +37,17 @@ conexao.close()
 # Crie a janela principal
 janela = tk.Tk()
 
-janela.geometry("500x300")
+janela.geometry("600x400")
 
 # Adicione um título à janela
-janela.title("Minha Interface")
+janela.title("Almoxarifado")
 
-# Crie um botão
-botao = tk.Button(janela, text="Clique aqui!")
+button_1 = tk.Button(janela, text="Inserir Item", width=10, height=1, command=open_window_1)
 
-# Adicione o botão à janela
-botao.pack()
+button_1.place(x=50, y=20)
 
+button_2 = tk.Button(janela, text="Remover Item", width=10, height=1, command=open_window_2)
+
+button_2.place(x=128, y=20)
 # Inicie o loop principal da interface
 janela.mainloop()
